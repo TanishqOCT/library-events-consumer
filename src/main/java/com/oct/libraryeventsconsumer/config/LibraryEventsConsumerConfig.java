9335d4a6e4715672089b611adf7bdbb14bcffa18
tanishq <tanishq.bhardwaj-ct@kwench.in>
2023-03-17 14:24:31 +0530

    configured consumer on kafka broker

---
+++ src/main/java/com/oct/libraryeventsconsumer/config/LibraryEventsConsumerConfig.java
@@ -0,0 +1,9 @@
+package com.oct.libraryeventsconsumer.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+
+@Configuration
+@EnableKafka
+public class LibraryEventsConsumerConfig {
+}
